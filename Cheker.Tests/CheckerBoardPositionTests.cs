d90912eb9cb0de36798fc58495c7fd5e047b6493
NilanSeon <148245767+EileenSchultz@users.noreply.github.com>
2026-04-28 16:29:34 +0300

    upd:вроде все и вроде работает

--- Cheker.Tests/CheckerBoardPositionTests.cs
+++ Cheker.Tests/CheckerBoardPositionTests.cs
@@ -57,5 +57,47 @@ public class CheckerBoardPositionTests
         Assert.Throws<ArgumentOutOfRangeException>(() => new CheckerBoardPosition(x, y)); 
     }
     
+    [Theory] //разные параметры проверяем
+    [InlineData(5, 1)] 
+    [InlineData(4, 4)] 
+    [InlineData(1, 6)]
+    [InlineData(8, 8)]
+   
+    public void NormalParemeter(byte x, byte y)
+    {
+        var position = new CheckerBoardPosition(x, y);
+        
+        Assert.Equal(x, position.X);
+        Assert.Equal(y, position.Y);
+    }
+    
+    [Theory] //опять невалидные параметры
+    [InlineData("W1")]
+    [InlineData("BB")]
+    [InlineData("...")]
+    [InlineData(" ")]
+    public void ParsingReturnFalse(string input)
+    {
+        var good = CheckerBoardPosition.TryParse(input, null, out var result);
+
+        Assert.False(good);
+        Assert.Null(result);
+    }
+    
+    [Theory] //тест парсинга валидных параметров
+    [InlineData("F5", 6, 5)]
+    [InlineData("B4", 2, 4)] 
+    [InlineData("H6", 8, 6)] 
+    public void ParsingValidInput(string input, byte x, byte y) 
+    {
+     
+        bool good = CheckerBoardPosition.TryParse(input, null, out var result); // Вызываем TryParse
+        
+        Assert.True(good); 
+        Assert.NotNull(result); 
+        Assert.Equal(x, result.X); 
+        Assert.Equal(y, result.Y); 
+    }
+    
     
 }
